f711b41743d27f69c330fa2b1043576a30c36c13
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-04-09 10:03:02 +0530

    reading file asynchronously

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const readline = require('readline')
+// const readline = require('readline')
+const fs = require('fs');
+
 // const rl = readline.createInterface({
 //     input:process.stdin,
 //     output:process.stdout
@@ -17,10 +19,19 @@ const readline = require('readline')
 //we can not use javascript files read and write but
 //when we use the javascritp in node.js.node js provides 
 //some apis which we can use to read and write files
-const fs = require('fs');
+// const fs = require('fs');
 
-let textIn = fs.readFileSync('./files/input.txt','utf-8');
-console.log(textIn)
+// let textIn = fs.readFileSync('./files/input.txt','utf-8');
+// console.log(textIn)
 
-let content = `Date read from input.txt: ${textIn} \nDate Created ${new Date()}`
-fs.writeFileSync('./files/output.txt', content);
+// let content = `Date read from input.txt: ${textIn} \nDate Created ${new Date()}`
+// fs.writeFileSync('./files/output.txt', content);
+
+fs.readFile('./files/start.txt','utf-8',(error1,data1)=>{
+    console.log(data1);
+    fs.readFile(`./files/${data1}.txt`,'utf-8',(error2,data2)=>{
+        console.log(data2);
+    })
+})
+
+console.log('Reading file...');
