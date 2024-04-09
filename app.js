8f14867f5e79b3ac7ab31d2bf2796f0d36f99192
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-04-09 22:35:31 +0530

    routing to html response

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-// const readline = require('readline')
+const readline = require('readline');
 const fs = require('fs');
+const http = require('http');
+
 
 // const rl = readline.createInterface({
-//     input:process.stdin,
+//      input:process.stdin,
 //     output:process.stdout
-// });
+//  });
 
 // rl.question("Please enter the name:",(name)=>{
 //     console.log("you entered: "+name);
 //     rl.close();
 // })
-// rl.on('close',()=>{
-//     console.log("Inter face closed");
-//     process.exit(0);
-// })
+//  rl.on('close',()=>{
+//      console.log("Inter face closed");
+//      process.exit(0);
+//  })
 
 
 //we can not use javascript files read and write but
@@ -27,17 +29,48 @@ const fs = require('fs');
 // let content = `Date read from input.txt: ${textIn} \nDate Created ${new Date()}`
 // fs.writeFileSync('./files/output.txt', content);
 
-fs.readFile('./files/start.txt','utf-8',(error1,data1)=>{
-    console.log(data1);
-    fs.readFile(`./files/${data1}.txt`,'utf-8',(error2,data2)=>{
-        console.log(data2);
-        fs.readFile('./files/append.txt','utf-8',(error3,data3)=>{
-            console.log(data3);
-            fs.writeFile('./files/output.txt',`${data2}\n\n${data3}\n\nDate created ${new Date()}`,()=>{
-                console.log('File writen successfully');
-            })
-        })
-    })
-})
+// fs.readFile('./files/start.txt','utf-8',(error1,data1)=>{
+//     console.log(data1);
+//     fs.readFile(`./files/${data1}.txt`,'utf-8',(error2,data2)=>{
+//         console.log(data2);
+//         fs.readFile('./files/append.txt','utf-8',(error3,data3)=>{
+//             console.log(data3);
+//             fs.writeFile('./files/output.txt',`${data2}\n\n${data3}\n\nDate created ${new Date()}`,()=>{
+//                 console.log('File writen successfully');
+//             })
+//         })
+//     })
+// })
 
-console.log('Reading file...');
+// console.log('Reading file...');
+
+const html = fs.readFileSync('./Template/index.html','utf-8')
+const contact = fs.readFileSync('./Template/contact.html','utf-8')
+
+// creating simple web server
+
+const server = http.createServer((request,response)=>{
+    // response.end(html)
+    // console.log('A new request received');
+    // // console.log(response)
+    let path = request.url;
+    
+    if(path === '/' || path.toLocaleLowerCase() === '/home'){
+        response.end(html);
+    }
+    else if(path.toLocaleLowerCase() === '/about'){
+        response.end('Hello this is about page')
+    }
+    else if(path.toLocaleLowerCase() === '/contact'){
+        response.end(contact)
+    }
+    else {
+        response.end('error:404 Page Not Found')
+    }
+});
+
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Server has started');
+});
+
