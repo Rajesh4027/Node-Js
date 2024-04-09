9c5ee04726ece2cad62fe974b72c863bee12b4a5
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-04-09 10:14:23 +0530

    sucessfully created read and wrie file by using node js

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ fs.readFile('./files/start.txt','utf-8',(error1,data1)=>{
     console.log(data1);
     fs.readFile(`./files/${data1}.txt`,'utf-8',(error2,data2)=>{
         console.log(data2);
+        fs.readFile('./files/append.txt','utf-8',(error3,data3)=>{
+            console.log(data3);
+            fs.writeFile('./files/output.txt',`${data2}\n\n${data3}\n\nDate created ${new Date()}`,()=>{
+                console.log('File writen successfully');
+            })
+        })
     })
 })
 
